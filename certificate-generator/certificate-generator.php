dc29527ecfe6e5d9653baa17086f89bcf726383d
Fadzilah Rafiq <fadzilahrafiq@gmail.com>
2024-04-15 01:14:47 +0800

    Update certification logic

--- certificate-generator/certificate-generator.php
+++ certificate-generator/certificate-generator.php
@@ -10,42 +10,81 @@
  * Text Domain: certificate-generator
  */
 
- require_once( plugin_dir_path( __FILE__ ) . 'fpdf/fpdf.php' );
+//  require_once( plugin_dir_path( __FILE__ ) . 'setaFPDF/autoload.php' );
+// require_once( 'setaFPDF/autoload.php' );
+// require_once( 'setaFPDF/SetaFpdf.php' );
+require_once( 'fpdf186/fpdf.php' );
 
- function generate_certificate( $custom_text ) {
+// use setasign\SetaFpdf\SetaFpdf;
 
-  // Path to the template PDF file (replace with your actual path)
-  $template_path = plugin_dir_path( __FILE__ ) . 'assets/CERT MYCOR.pdf';
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-  // Create a new FPDI instance
-  $pdf = new FPDI();
+// function generate_certificate( $custom_text ) {
 
-  // Add a new page with the size of the template
-  $pdf->AddPage(filesize($template_path));
+//   // Path to the template PDF file (replace with your actual path)
+//   $template_path = 'assets/CERT MYCOR.pdf';
+//   echo "<script>console.log('template path -> ', '".$template_path."' );</script>";
 
-  // Import the template PDF content onto the newly created page
-  $pdf->setSourceFile($template_path);
-  $template_page = $pdf->ImportPage(1);
-  $pdf->useTemplate($template_page, 0, 0);
+//   // Create a new FPDI instance
+//   $pdf = new SetaFpdf();
 
-  // Set font and size for the custom text (replace with your desired settings)
-  $pdf->SetFont('Arial', 'B', 16);
+//   // Add a new page with the size of the template
+//   $pdf->AddPage(filesize($template_path));
 
-  // Calculate text width
-  $text_width = $pdf->GetStringWidth($custom_text);
+//   // Import the template PDF content onto the newly created page
+//   $pdf->setSourceFile($template_path);
+//   $template_page = $pdf->ImportPage(1);
+//   $pdf->useTemplate($template_page, 0, 0);
 
-  // Calculate horizontal center position based on page width and text width
-  $x_center = ($pdf->GetPageWidth() - $text_width) / 2;
+//   // Set font and size for the custom text (replace with your desired settings)
+//   $pdf->SetFont('Arial', 'B', 16);
 
-  // Adjust vertical position for the text as needed (replace with your desired position)
-  $y_position = 50;
+//   // Calculate text width
+//   $text_width = $pdf->GetStringWidth($custom_text);
 
-  // Add the custom text at the center of the first page
-  $pdf->Text($x_center, $y_position, $custom_text);
+//   // Calculate horizontal center position based on page width and text width
+//   $x_center = ($pdf->GetPageWidth() - $text_width) / 2;
 
-  // Generate the final PDF output as a string
-  $pdf_output = $pdf->Output('S');
+//   // Adjust vertical position for the text as needed (replace with your desired position)
+//   $y_position = 50;
 
-  return $pdf_output;
+//   // Add the custom text at the center of the first page
+//   $pdf->Text($x_center, $y_position, $custom_text);
 
-}
+//   // Generate the final PDF output as a string
+//   $pdf_output = $pdf->Output('S');
+
+//   return $pdf_output;
+
+// }
+
+function generate_certificate( $custom_text ) {
+
+    $template_path = plugin_dir_path( __FILE__ ) . 'assets/CERT_MYCOR.jpeg';
+
+    // $template_pdf = new FPDF();
+    $new_pdf = new FPDF();
+
+    // $template_pdf->setSourceFile($template_path);
+    // $template_page = $template_pdf->ImportPage(1);  // Assuming text is on the first page
+    // $size = $template_pdf->GetPageSize($template_page);
+
+    $new_pdf->AddPage('L', [3508, 2480], 0 );
+
+    $new_pdf->Image($template_path, 0, 0, 3508, 2480);
+    
+    $new_pdf->SetFont('Arial', 'B', 250); // Example: Arial, bold, size 16
+
+    $text_width = $new_pdf->GetStringWidth($custom_text);
+    $x_center = (($new_pdf->GetPageWidth()) / 2) - $text_width;  // Calculate horizontal center based on new page width
+    $y_position = ($new_pdf->GetPageHeight())/2; //50;  // Adjust vertical position as needed
+    $new_pdf->Text($x_center, $y_position, $custom_text);
+
+    // $pdf_output = $new_pdf->Output('custom_certificate.pdf', 'F'); 
+    $pdf_output = $new_pdf->Output('S'); 
+  
+    return $pdf_output;
+  
+  }
---
+++ certificate-generator/setaFPDF/StateBuffer/StateBufferInterface.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of SetaFPDF
+ *
+ * @package   setasign\SetaFpdf
+ * @copyright Copyright (c) 2020 Setasign GmbH & Co. KG (https://www.setasign.com)
+ * @license   http://opensource.org/licenses/mit-license The MIT License
+ */
+
+namespace setasign\SetaFpdf\StateBuffer;
+
+use setasign\SetaFpdf\CleanupInterface;
+
+interface StateBufferInterface extends CleanupInterface
+{
+    /**
+     * Resets the state.
+     */
+    public function reset();
+
+    /**
+     * Stores the current state.
+     */
+    public function store();
+
+    /**
+     * Restores the last stored state.
+     */
+    public function restore();
+}
